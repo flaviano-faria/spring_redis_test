74b8d19292196b372da0a06d21306430baf72d08
flaviano-faria <flsilv89@yahoo.com.br>
2025-04-22 23:23:29 -0300

    Implementing doGet() method

--- src/test/java/com/springredis/util/request/Request.java
+++ src/test/java/com/springredis/util/request/Request.java
@@ -1,4 +1,18 @@
 package com.springredis.util.request;
 
+import io.restassured.response.Response;
+
+import static io.restassured.RestAssured.given;
+
 public class Request {
+
+    public String doGet(String url, String path) {
+        Response response = given()
+                .when()
+                .get(path)
+                .then()
+                .extract()
+                .response();
+        return response.getBody().asString();
+    }
 }
