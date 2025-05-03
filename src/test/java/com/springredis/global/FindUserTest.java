9ed8455531ec4a00bb4bbb170b635ef1a6b51c2c
flaviano-faria <flsilv89@yahoo.com.br>
2025-05-02 22:22:33 -0300

    Improving unit tests

--- src/test/java/com/springredis/global/FindUserTest.java
+++ src/test/java/com/springredis/global/FindUserTest.java
@@ -3,16 +3,37 @@ package com.springredis.global;
 import com.springredis.util.json.JSonUtil;
 import com.springredis.util.request.Request;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+import io.cucumber.datatable.DataTable;
+
+import java.util.List;
+
 
 public class FindUserTest {
 
     private int numberOfRecords;
 
     @Given("Given  I have users in database:")
-    public void given_I_have_users_in_database() {
+    public void given_I_have_users_in_database(List<User> listUser) {
         Request request = new Request();
         String response = request.doGet("http://localhost:8080/userservice/", "");
+        System.out.println();
+    }
 
+    @When("When send find all user request")
+    public void when_send_find_all_user_request(List<User> listUser) {
+        Request request = new Request();
+        String response = request.doGet("http://localhost:8080/userservice/", "");
+    }
+
+    @Then("Then I should have retrieved correctly all users")
+    public void then_I_have_retrieved_correctly_all_users() {
+        Request request = new Request();
+        System.out.println();
     }
 
 }
--- src/test/resources/FindUser.feature
+++ src/test/resources/FindUser.feature
@@ -1,5 +1,13 @@
+Feature: find user in redis
+
 Scenario Outline: Find user
 Given Given  I have users in database:
-
+| id   | name      |
+| <id> | <name>    |
 When  When send find all user request
 Then  Then I should have retrieved correctly all users
+
+Examples:
+| id   | name      |
+| 12   | testname1 |
+| 20   | testname2 |
